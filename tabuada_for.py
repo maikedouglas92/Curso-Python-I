397d8d6bc1d524281abed7d99a4716e6e4efe5f0
maikedouglas92 <maikedouglas92>
2026-09-19 09:30:33 -0300

    Primeira Branch

--- tabuada_for.py
+++ tabuada_for.py
@@ -5,4 +5,4 @@ numero = int(input("Digite um número: "))
 
 for i in range(1, 11):
     resultado = numero * i
-    print(f"{numero} x {i} = {resultado}")
+    print(f"{numero} x {i} = {numero * i}")
